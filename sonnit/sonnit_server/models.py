b5115f2e804deb232469f35cfc016513de8e66e4
Sonnit <sonnit@ip-172-31-13-59.ec2.internal>
2016-11-30 03:22:39 +0000

    Working on sonnet voting

--- sonnit/sonnit_server/models.py
+++ sonnit/sonnit_server/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Sonnet(models.Model):
+	text = models.CharField(max_length=1400)
+	
+	def get_score(self):
+		upvotes = Score.objects.filter(sonnet=self, value=True).count()
+		downvotes = Score.objects.filter(sonnet=self, value=False).count()
+		return upvotes - downvotes
+
+
+class Score(models.Model):
+	sonnet = models.ForeignKey(Sonnet)
+	ip = models.CharField(max_length=40)
+	value = models.BooleanField()
