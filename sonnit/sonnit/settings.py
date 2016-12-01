35cdc0a319ae36353fa83af83436293c6fd3025b
Sonnit <sonnit@ip-172-31-13-59.ec2.internal>
2016-12-01 08:27:03 +0000

    Getting the server front end going and looking pretty

--- sonnit/sonnit/settings.py
+++ sonnit/sonnit/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'sonnit',
 	'USER': 'django',
 	'PASSWORD': 'SonnitServerPassword#',
-	'HOST': 'localhost',
+	'HOST': '127.0.0.1',
 	'PORT': '3306',
 	'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
